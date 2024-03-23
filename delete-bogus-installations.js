fa516040eec3423587ce350e198c89a6e41ebf8c
Johannes Schindelin <johannes.schindelin@gmx.de>
2024-03-23 21:32:40 +0100

    delete-bogus-installations: keep installation for embargoed builds
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- delete-bogus-installations.js
+++ delete-bogus-installations.js
@@ -5,9 +5,18 @@
     process.env.GITHUB_APP_ID = localSettings.Values.GITHUB_APP_ID
     process.env.GITHUB_APP_PRIVATE_KEY = localSettings.Values.GITHUB_APP_PRIVATE_KEY
 
+    const sha256 = text => {
+        const crypto = require('crypto')
+        return crypto.createHash('sha256').update(text).digest('hex')
+    }
+
     const gitHubRequestAsApp = require('./GitForWindowsHelper/github-api-request-as-app')
     const answer = await gitHubRequestAsApp(console, 'GET', '/app/installations?per_page=100')
-    for (const e of answer.filter(e => e.account.login !== 'git-for-windows' && e.account.login !== 'dscho')) {
+    for (const e of answer.filter(e =>
+        e.account.login !== 'git-for-windows' &&
+        e.account.login !== 'dscho' &&
+	sha256(e.account.login) !== 'c84dfa28e1830ba79dfc165165e328999ba448aedfe92517c00b0005cea10c82'
+    )) {
         console.log(`Deleting installation ${e.id} for ${e.account.login}`)
         await gitHubRequestAsApp(console, 'DELETE', `/app/installations/${e.id}`)
     }
